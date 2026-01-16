b73da5f9a5a16bcea8e264a4fd8792fdac0db7f1
savita <savitagsathe@gmail.com>
2026-01-16 19:19:15 +0530

    16 jan 2026 all files

--- src/ex_01_java_Task/Practice.java
+++ src/ex_01_java_Task/Practice.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 
 public class Practice {
     public static void main(String[] args) {
+       String s="test";
+        String b="done";
         int marks=12;
+        char c=' ';
    if (marks>=90&&marks<=100){
 
    }
