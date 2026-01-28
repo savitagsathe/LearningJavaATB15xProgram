bf6ea713b7aaabaae6fbd5d3aa7db8e77823b2e9
savita <savitagsathe@gmail.com>
2026-01-28 23:12:24 +0530

    23 jan Java test

--- src/ex_01_java_Task/Practice.java
+++ src/ex_01_java_Task/Practice.java
@@ -4,13 +4,18 @@ import java.util.Scanner;
 
 public class Practice {
     public static void main(String[] args) {
-       String s="test";
-        String b="done";
-        int marks=12;
-        char c=' ';
-   if (marks>=90&&marks<=100){
 
+        int n=5;
+        for(int i=1;i<n;i++){
+            for(int space=1;space<n-i;space++){
+                System.out.println(" ");
+            }
+            for (int j=1;j<i*2-1;j++)
+            {
+                System.out.print("*");
+            }
+        }
    }
     }
-}
+
 
